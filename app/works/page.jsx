a2cd66fb378f83efa24718f23bf86070eb803c4a
javadhemmati1374 <javadhemati1374@gmail.com>
2024-08-27 23:16:02 +0330

    add content in works page

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,7 +1,39 @@
+import Link from "next/link";
+
 export default function Works() {
   return (
     <main>
-      <h1>Works Page</h1>
+      <section className="h-[84vh]">
+        <div className="h-[80rem] w-full dark:bg-black bg-white dark:bg-grid-white/[0.8] bg-grid-black/[0.1] relative ">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 !z-0 dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
+
+          {/* hero */}
+          <div className="container py-4 pt-48 mx-auto">
+            <div className="flex flex-col mx-auto xl:mx-0 xl:text-left">
+              <div className="flex gap-4 justify-start items-center text-[64px] font-bold text-secondary z-0">
+                With RoyaBiz
+              </div>
+              <div className="text-[64px] font-bold text-secondary -mt-4 z-0">
+                Build your royal business
+              </div>
+              <p className="text-[24px] mt-2 leading-8 max-w-[850px] mx-auto xl:mx-0 z-0">
+                Helping brands to stand out in the digital era. Together we will
+                set the new status quo. No nonsense, always on cutting edge.
+              </p>
+            </div>
+            {/* buttons */}
+            <div className="flex flex-col items-end -mt-28">
+              <Link
+                href="/contact"
+                className="font-bold italic py-24 px-12 rounded-full bg-foreground text-primary-foreground text-[24px] hover:bg-primary transition-all z-[1]"
+              >
+                Get Started
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
